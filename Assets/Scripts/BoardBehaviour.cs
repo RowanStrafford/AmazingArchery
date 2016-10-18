838877b6ee0e220363e6d84279cb768d9f38455e
RowanStrafford <rowan.strafford@ymail.com>
2016-10-18 18:00:55 +0100

    Added Board Behaviour script

---
+++ Assets/Scripts/BoardBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardBehaviour : MonoBehaviour {
+
+    private bool moveDown = false;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	    if(moveDown)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y - 1.0f, transform.position.z), Time.deltaTime * 2.0f);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject, 1.5f);
+        Invoke("SetMoveDown", 2.0f);
+    }
+
+    void SetMoveDown()
+    {
+        moveDown = true;
+    }
+}
